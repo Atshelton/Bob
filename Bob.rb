83c0f027ea193c0acb0de7710262b06824a45b59
Alex Shelton <tbfunkymonkey@gmail.com>
2016-06-28 23:10:49 -0400

    "I am very glad to be done with Bob"

--- Bob.rb
+++ Bob.rb
@@ -1,11 +1,13 @@
 class Bob
-
-def initialize(argument)
-  @argument = argument
-end
-
-
-
-
-
+  def hey(remark)
+    if remark.strip == ''
+      'Fine. Be that way!'
+    elsif remark == remark.upcase && remark.count("A-z") != 0
+      'Whoa, chill out!'
+    elsif remark[-1] == '?'
+       'Sure.'
+    else
+      'Whatever.'
+    end
+  end
 end
